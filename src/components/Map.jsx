cbce9d1ed46fef65619917f4dbe38eb0dbbc00a3
simonfotnt <simonpiar98@gamil.com>
2025-04-07 11:52:36 +0600

    added map section

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Incidents from './../pages/Incidents';
 
 function Map() {
   return (
-    <div>
-        
-    </div>
+    <>
+    
+        <div className='max-w-xl   mx-auto py-8'>
+            <div className=''>
+                <h2 className='text-2xl font-ones font-bold'>Where's the Incidents?</h2>
+                <p className='font-ones text-sm text-secondary my-4'>
+                Enter a GPS, address. or pin point on the map. Try to be as accurate as
+                 possible, or click:Jurisdiction Wide
+                </p>
+            </div>
+            <div>
+                <img src="img/map2.png" alt="" />
+            </div>
+        </div>
+    </>
   )
 }
 
